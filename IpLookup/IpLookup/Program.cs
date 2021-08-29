0d9992448f7693f764649e2ec569be2eb7b458d7
Zabor <54038884+Zabor40@users.noreply.github.com>
2021-08-29 15:39:03 +0200

    Add files via upload

---
+++ IpLookup/IpLookup/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace IpLookup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string c = @" _  ____  _     ____  ____  _  __ _     ____ 
+/ \/  __\/ \   /  _ \/  _ \/ |/ // \ /\/  __\
+| ||  \/|| |   | / \|| / \||   / | | |||  \/|
+| ||  __/| |_/\| \_/|| \_/||   \ | \_/||  __/
+\_/\_/   \____/\____/\____/\_|\_\\____/\_/   
+                                         ";
+            WebClient webClient1 = new WebClient();
+            Console.Title = "IpLookup | made by Zabor40";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(c);
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Enter ip here: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(webClient1.DownloadString("https://api.hackertarget.com/geoip/?q=" + a));
+            Console.ReadKey();
+        }
+    }
+}
